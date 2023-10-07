52554ed78f4d90a2cfb0e6cf8655665c44dd25eb
rizkyfakhriansyah <najwafahri123@gmail.com>
2023-10-08 02:05:04 +0700

    if,else,nestedif

---
+++ script.js
@@ -0,0 +1,32 @@
+//Menggunakan if
+var nama="fahri";
+    if (nama=="fahri")
+    {
+        console.log("Kamu Berhasil Menggunakan if");
+    }
+
+
+
+//Menggunakan if else
+var nama="fahri";
+    if (nama=="aa")
+        console.log("Kamu Berhasil Menggunakan if");
+    else
+        console.log("Kamu Salah ");
+
+
+
+
+//Menggunakan nested if
+var nama1 = "fahri";
+var nama2 = "wawa";
+
+        if(nama1 == "fahri"){
+            if(nama2 == "fafa"){
+                console.log("Kamu Berhasil");
+            } else {
+                console.log("Salah Satu Ada Yang Salah");
+            }
+            } else {
+                console.log("Keduanya Salah");
+             }
