ae61fdb13e11cb5322fc0cc57207bb89686b04fd
rizkyfakhriansyah <146195760+rizkyfakhriansyah@users.noreply.github.com>
2023-10-08 02:21:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let x="bagus" 
+switch(x){    
+   case "bagus":
+    console.log("kamu adalah bagus")    
+     break;    
+   case "bukan bagus":
+    console.log("kamu bukan bagus")        
+     break;    
+   default :
+    console.log("Kamu Orang asing")
+}
