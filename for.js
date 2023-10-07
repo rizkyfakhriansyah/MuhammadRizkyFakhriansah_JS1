cd44627bdf6db40015df0c11386ac9b6ed72dae3
rizkyfakhriansyah <146195760+rizkyfakhriansyah@users.noreply.github.com>
2023-10-08 02:26:27 +0700

    for statement

---
+++ for.js
@@ -0,0 +1,9 @@
+for(let i = 0; i < 10; i++){
+    console.log("Ini adalah " + i )
+}
+
+var bahasa = ["Javascript", "HTML", "CSS", "Typescript"];
+
+for(i = 0; i < bahasa.length; i++){
+    console.log(i+". "+ bahasa[i] );
+}
