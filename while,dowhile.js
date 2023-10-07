a46f44372042ddfb4246adc5bf27e674cea9e2e0
rizkyfakhriansyah <146195760+rizkyfakhriansyah@users.noreply.github.com>
2023-10-08 02:35:24 +0700

    Add files via upload

---
+++ while,dowhile.js
@@ -0,0 +1,15 @@
+//menggunakan while
+var i=0;
+while (i<=10)
+{
+   console.log("Saya fahri"+i);
+   i++;
+}
+
+
+//menggunakan do while
+m = 1
+do {
+   console.log("saya fahri "+m)
+   m++
+}while(m<=10)
